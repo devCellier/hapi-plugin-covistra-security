7c5f0d077259c634f7f0dee481bb75e21f3607d2
Joel Grenon <joelgrenon@covistra.com>
2015-09-21 13:58:05 -0400

    Use Cmbf dependency mechanism instead of Hapi

--- index.js
+++ index.js
@@ -17,68 +17,65 @@
 
 var _ = require('lodash');
 
+exports.deps = ['covistra-system', 'covistra-mongodb'];
+
 exports.register = function (server, options, next) {
 
-    server.dependency(['covistra-system', 'covistra-mongodb'], function(plugin, done) {
+    var config = server.plugins['hapi-config'].CurrentConfiguration;
+    var log = server.plugins['covistra-system'].systemLog.child({plugin: 'security'});
+    var Router = server.plugins['covistra-system'].Router;
+    var resolveDeps = server.plugins['covistra-system'].resolveDeps;
 
-        var config = server.plugins['hapi-config'].CurrentConfiguration;
-        var log = server.plugins['covistra-system'].systemLog.child({plugin: 'security'});
-        var Router = server.plugins['covistra-system'].Router;
-        var resolveDeps = server.plugins['covistra-system'].resolveDeps;
+    log.debug("Registering the security plugin");
 
-        log.debug("Registering the security plugin");
+    server.expose('Users', require('./lib/users')(server, log, config));
+    server.expose('Groups', require('./lib/groups')(server, log, config));
+    server.expose('Applications', require('./lib/applications')(server, log, config));
+    server.expose('Tokens', require('./lib/tokens')(server, log, config));
 
-        server.expose('Users', require('./lib/users')(server, log, config));
-        server.expose('Groups', require('./lib/groups')(server, log, config));
-        server.expose('Applications', require('./lib/applications')(server, log, config));
-        server.expose('Tokens', require('./lib/tokens')(server, log, config));
+    server.auth.strategy('simple', 'basic', { validateFunc: require('./lib/basic_validate')(server, log, config) });
 
-        server.auth.strategy('simple', 'basic', { validateFunc: require('./lib/basic_validate')(server, log, config) });
+    // Register our auth strategy
+    server.auth.strategy('token', 'bearer-access-token', { validateFunc: require('./lib/validate_token')(server, log, config)});
 
-        // Register our auth strategy
-        server.auth.strategy('token', 'bearer-access-token', { validateFunc: require('./lib/validate_token')(server, log, config)});
+    // Set our strategy
+    server.auth.strategy('session', 'cookie', _.defaults(config.get('plugins:security:session') || {}, {
+        password: 'D@H@H23e@(*#E@HDSADAS@', // cookie secret
+        cookie: '_cmbf_auth_session', // Cookie name
+        redirectTo: '/login', // Let's handle our own redirections
+        isSecure: process.env.NODE_ENV === 'production', // required for non-https applications
+        appendNext: true,
+        ttl: 30 * 24 * 60 * 60 * 1000 // Set session to 30 days
+    }));
 
-        // Set our strategy
-        server.auth.strategy('session', 'cookie', _.defaults(config.get('plugins:security:session') || {}, {
-            password: 'D@H@H23e@(*#E@HDSADAS@', // cookie secret
-            cookie: '_cmbf_auth_session', // Cookie name
-            redirectTo: '/login', // Let's handle our own redirections
-            isSecure: process.env.NODE_ENV === 'production', // required for non-https applications
-            appendNext: true,
-            ttl: 30 * 24 * 60 * 60 * 1000 // Set session to 30 days
-        }));
-
-        server.select('admin').views({
-            engines: {
-                html: require('handlebars')
-            },
-            relativeTo: __dirname,
-            path: 'views',
-            layout: true,
-            partialsPath: 'views/partials',
-            layoutPath: "views/layouts"
-        });
-
-        // Register routes
-        Router.routes(server, __dirname, './routes');
-
-        // Lazy plugin resolution to avoid circular dependencies issue
-        resolveDeps('covistra-admin').then(function(adminPlugin) {
-            log.debug("Installing our admin interface");
-
-            // Register our admin interface elements
-            adminPlugin.adminService.registerModule('security-admin', require('./admin/module'));
-
-            log.info("Security Admin interface has been successfully installed");
-
-        }).catch(function() {
-            log.warn("Unable to resolve admin plugin. No interface will be created");
-        });
-
-        //NOTE: Must call done before install the admin as security plugin is a dependency of admin plugin!
-        done();
+    server.select('admin').views({
+        engines: {
+            html: require('handlebars')
+        },
+        relativeTo: __dirname,
+        path: 'views',
+        layout: true,
+        partialsPath: 'views/partials',
+        layoutPath: "views/layouts"
     });
 
+    // Register routes
+    Router.routes(server, __dirname, './routes');
+
+    // Lazy plugin resolution to avoid circular dependencies issue
+    resolveDeps('covistra-admin').then(function(adminPlugin) {
+        log.debug("Installing our admin interface");
+
+        // Register our admin interface elements
+        adminPlugin.adminService.registerModule('security-admin', require('./admin/module'));
+
+        log.info("Security Admin interface has been successfully installed");
+
+    }).catch(function() {
+        log.warn("Unable to resolve admin plugin. No interface will be created");
+    });
+
+    //NOTE: Must call done before install the admin as security plugin is a dependency of admin plugin!
     next();
 };
 
